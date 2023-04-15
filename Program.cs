e4639574f908624d0c7a159101cae992708af624
Olga Komar <komar_ol@mail.ru>
2023-04-15 17:49:08 +0300

    added the solution to the third task

--- Program.cs
+++ Program.cs
@@ -4,3 +4,25 @@
 //6 -> да
 //7 -> да
 //1 -> нет
+
+int NumDayEntry()
+{
+    Console.Write("Введите цифру, дня недели= ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Weekday(int numDay)
+{
+    if (numDay < 1 || numDay > 7)
+    {
+        Console.WriteLine("В неделе всего 7 дней,неверный ввод");
+    
+    }
+    else
+    {
+        if (numDay >= 6) { Console.WriteLine("Сегодня выходной!!!"); }
+        else{ Console.WriteLine("Сегодны будний день!");}
+    }
+}
+
+Weekday(NumDayEntry());
